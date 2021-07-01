cf6bf9bca657ba0d96f3677a1677d7b861ae3c79
Amirhossein <amirhosseinleo@gmail.com>
2021-07-02 03:22:48 +0430

    Add username to db

--- statistics_bot/models.py
+++ statistics_bot/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Boolean, Float
+from sqlalchemy import Column, Integer, Boolean, Float, String
 from .database import Base
 
 # Create model for database
@@ -7,6 +7,7 @@ class User(Base):
 
     _id = Column(Integer, primary_key=True)
     chat_id = Column(Integer)
+    username = Column(String)
     plot_mode = Column(Boolean, default=True)
     digits = Column(Integer, default=6)
     alpha = Column(Float, default=0.05)
