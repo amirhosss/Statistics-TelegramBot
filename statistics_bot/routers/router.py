3a7c557f4d1e1d3621d4ed42ddccec428900883e
Amirhossein <amirhosseinleo@gmail.com>
2021-08-01 21:47:58 +0430

    Remove async

--- statistics_bot/routers/router.py
+++ statistics_bot/routers/router.py
@@ -9,18 +9,18 @@ WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
 router = APIRouter()
 
 @router.post('/' + main.TOKEN)
-async def get_message(request: Request):
-    json_string = await request.json()
+def get_message(request: Request):
+    json_string = request.json()
     update = types.Update.de_json(json_string)
     main.bot.process_new_updates([update])
     return 200
     
 
 @router.get('/setwebhook')
-async def webhook():
+def webhook():
     main.bot.remove_webhook()
-    await main.bot.set_webhook(url=WEBHOOK_URL + main.TOKEN)
-    return "!", 200
+    main.bot.set_webhook(url=WEBHOOK_URL + main.TOKEN)
+    return '!', 200
     
 
 
