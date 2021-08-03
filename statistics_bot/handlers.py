c0d0b62adadf26acf618751f7f8b449875de1031
Amirhossein <amirhosseinleo@gmail.com>
2021-08-03 23:28:29 +0430

    Change distributions handler

--- statistics_bot/handlers.py
+++ statistics_bot/handlers.py
@@ -18,14 +18,14 @@ def start(bot, message):
 def help(bot, message):
     bot.reply_to(message, 
                    '&#x1F4C9 Variance\ne.g. (X&#x2080=5, X&#x2081=42 ..., X&#x2099=70):\n<b>v 5 42 ... 70</b>\n\n'
-                 + '&#x1F4CA Normal distribution inverse\ne.g. (&#x03b1 = 0.05):\n<b>ni 0.05</b>\n'
-                 + '&#x1F4CA Normal distribution\ne.g. (x = 1):\n<b>n 1</b>\n' 
-                 + '&#x1F4CA t-student inverse\ne.g. (&#x3b1 = 0.05, &#x3bd = 9):\n<b>ti 0.05 9</b>\n'
-                 + '&#x1F4CA t-student\ne.g. (x = 1, v = 9):\n<b>t 1 9</b>\n\n' 
+                 + '&#x1F4CA Normal. Inverse survival function\ne.g. (&#x03b1 = 0.05):\n<b>n.isf 0.05</b>\n'
+                 + '&#x1F4CA Normal. Survival function\ne.g. (x = 1):\n<b>n.sf 1</b>\n' 
+                 + '&#x1F4CA t-student. Inverse survival function\ne.g. (&#x3b1 = 0.05, &#x3bd = 9):\n<b>t.isf 0.05 9</b>\n'
+                 + '&#x1F4CA t-student. Survival function\ne.g. (x = 1, v = 9):\n<b>t.sf 1 9</b>\n\n' 
                  + '&#x1F4C8 Normal test\ne.g. (X&#x305 = 14, &#x3BC = 13, &#x3C3 = 2, n = 10, (H&#x2081: &#x3BC&gt&#x3BC&#x2080) = &gt):\n<b>ntest 14 13 2 10 &gt</b>\n'
                  + '&#x1F4C8 t test\ne.g. (X&#x305 = 14, &#x3BC = 13, S = 2, n = 10, (H&#x2081: &#x3BC&lt&#x3BC &#x2080) = &lt):\n<b>ttest 14 13 2 10 &lt</b>\n\n'
                  + '&#x1F527 Send /plot to change the plot mode\n(default: on)\n' 
-                 + '&#x1F527 Send /alpha to set the alpha_value\n(default: 0.05)\n' 
+                 + '&#x1F527 Send /alpha to set the alpha_value for hypothesis tests\n(default: 0.05)\n' 
                  + '&#x1F527 Send /digits to set digits for\nanswers (default: 6)', 
                     parse_mode='HTML')
 
