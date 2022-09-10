8c0d28db2c7e0980555b2ee6022da76e295f30d3
amirhosss <amirhosseinleo@yahoo.com>
2022-09-10 05:09:53 +0430

    Host deleted from uvicorn config

--- main.py
+++ main.py
@@ -126,4 +126,4 @@ if __name__ == '__main__':
         bot.delete_webhook()
         bot.polling()
     else:
-        uvicorn.run('main:app', port=8000, host='0.0.0.0')
+        uvicorn.run('main:app', port=8000)
